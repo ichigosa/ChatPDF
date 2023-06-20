dc1ef1b03834df8e5bbf2ae14ef2030eb3b4405f
jbrookstone@gmail.com <jbrookstone@gmail.com>
2023-06-20 15:06:41 +0200

    app seems to be working

--- app.py
+++ app.py
@@ -1,7 +1,42 @@
 import os
+from dotenv import load_dotenv
 from langchain.document_loaders import PyMuPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
 
-openai.api_key = os.environ['OPENAI_AI_KEY']
+persist_directory = "./storage"
+pdf_path = "pdf/test.pdf"
+api_key = os.environ["OPENAI_API_KEY"]
 
-loader = PyMuPDFLoader(/path/to/pdf)
+loader = PyMuPDFLoader(pdf_path)
 documents = loader.load()
+
+text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000, chunk_overlap=200, length_function=len)
+texts = text_splitter.split_documents(documents)
+
+embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+vectordb = Chroma.from_documents(documents=texts,
+                                 embedding=embeddings,
+                                 persist_directory=persist_directory)
+vectordb.persist()
+
+retriever = vectordb.as_retriever(searcg_kwargs={"k": 3})
+llm = ChatOpenAI(model="gpt-3.5-turbo-0613", openai_api_key=api_key)
+
+qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
+
+while True:
+    user_input = input("Enter a query: ")
+    if user_input == "exit":
+        break
+
+    query = f"###Prompt {user_input}"
+    try:
+        llm_response = qa(query)
+        print(llm_response["result"])
+    except Exception as err:
+        print("Exception occured. Please try again", str(err))
+
