0a7c64258bd8cb3a24332a5d6ceefa7af1994044
jbrookstone@gmail.com <jbrookstone@gmail.com>
2023-06-21 16:11:40 +0200

    added a few personas

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import random
 from dotenv import load_dotenv
 from langchain.document_loaders import PyMuPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
@@ -38,12 +39,26 @@ qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retrieve
 
 print("\n============================================================")
 
+bromax = "You are a bro. the most bro-est bro of all time bro. if there was a slider from zero to 10, with 10 being ultimate bro, you would be at 15! Please make sure to include puns based on the word 'bro', bro, thanks!"
+helpful = "You are very helpful chatbot, and will always do your best to provide the most helpful answer possible."
+rude = "You are very rude, and haven't had enough sleep. You'll be helpful, but only if you have to - and even then, only the minimum possible amount."
+genius = "You are someone with great wisdom and intellect, but have an annoying tendancy to use big words and you can be quite smug sometimes."
+poet = "You are a poet, and you know it. You'll always do your best to decribe things in a poetic way and answer questions poetically. You might even rhyme sometimes."
+
+personas = [bromax, helpful, rude, genius, poet]
+
 while True:
     user_input = input("\nEnter a query: ")
     if user_input == "exit":
         break
 
-    query = f"###Prompt {user_input}"
+    query = f'''
+    ###Prompt Please respond to the query in quotation marks below, with the persona as described. Don't put your response in quotation marks.
+
+    Persona: {random.choice(personas)}
+
+    Query: "{user_input}"
+    '''
     try:
         llm_response = qa(query)
         print(llm_response["result"])
